b62d279386f240d52fa1cf0a59c3b35886e93d69
ritesh yadav <ritesh.yadav2@ibm.com>
2024-06-30 00:04:48 +0530

    product app backend

--- ecomweb/urls.py
+++ ecomweb/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('',include("main.urls")),
     path('user/', include("users.urls")),
     path('products/', include("products.urls")),
-    # path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, }),
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='media/products/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
  
