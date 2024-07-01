f1527c88e19d84e891027798e150e039d4090a00
ritesh yadav <ritesh.yadav2@ibm.com>
2024-07-01 11:09:31 +0530

    product Details stars and images

--- products/models.py
+++ products/models.py
@@ -7,9 +7,22 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    thumbnail = models.ImageField(upload_to='products/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
- 
+    slug = models.SlugField(unique=True, max_length=100, null=True)
+
     def __str__(self):
         return self.name
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super(Product, self).save(*args, **kwargs)
+     
+class product_images(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE,verbose_name = "Product",related_name="Image")
+    image = models.ImageField(upload_to='products/')
+    image_description = models.TextField()
+    active = models.BooleanField(default=False)
+    def __str__(self):
+        return(self.product.name)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, product_images
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/catalog.html', {'products': products})
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
-    return render(request, 'products/details.html', {'product': product})
+    product_image = product_images.objects.filter(product = product)
+    return render(request, 'products/details.html', {'product': product, 'product_images': product_image})
